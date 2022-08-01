b21256845746cbb83d396e1553de7ca890a8f04d
WhiteTestarossa <daniel.belokursky@gmail.com>
2022-08-01 20:43:12 +0300

    init: start drawings project

---
+++ Drawings/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Drawings
+//
+//  Created by Daniel Belokursky on 1.08.22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Drawings/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Drawings
+//
+//  Created by Daniel Belokursky on 1.08.22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
